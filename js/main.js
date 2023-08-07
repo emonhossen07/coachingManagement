24c82bde56f5c4ae643aeafa5812bfb5951e7e18
emonhossen07 <anashossainemon@gmail.com>
2023-08-08 04:46:13 +0600

    mixitup prblm fixing

--- js/main.js
+++ js/main.js
@@ -1,18 +1,81 @@
 
 //scrollbar navbar 
-let nav = document.querySelector (".navigation-wrap");
+let nav = document.querySelector(".navigation-wrap");
 window.onscroll = function () {
-    if(document.documentElement.scrollTop > 20){
+    if (document.documentElement.scrollTop > 20) {
         nav.classList.add("scroll-on");
-    }else{
+    } else {
         nav.classList.remove("scroll-on");
     }
 }
 //nav hide
 let navBar = document.querySelectorAll(".nav-link");
 let navCollapse = document.querySelector(".navbar-collapse.collapse");
-navBar.forEach(function(a){
-    a.addEventListener("click", function(){
+navBar.forEach(function (a) {
+    a.addEventListener("click", function () {
         navCollapse.classList.remove("show");
     })
 })
+
+
+// next page 
+
+
+// const nextPage = document.querySelectorAll(".courses");
+// nextPage.addEventListener("click", function(event) {
+//     console.log("hello world");
+// })
+
+
+
+// owl carousel
+
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items: 3,
+    loop: true,
+    margin: 10,
+    autoplay: true,
+    autoplayTimeout: 4000,
+    autoplayHoverPause: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 1
+        },
+        1000: {
+            items: 3
+        }
+    }
+});
+$('.play').on('click', function () {
+    owl.trigger('play.owl.autoplay', [2000])
+})
+$('.stop').on('click', function () {
+    owl.trigger('stop.owl.autoplay')
+})
+
+
+
+// mixitup('#mix-wrapper', {
+//     load: {
+//         sort: 'order:asc'
+//     },
+//       animation: {
+//       effects: 'fade rotateZ(-180deg)',
+//       duration: 700
+//     },
+//     classNames: {
+//       block: 'programs',
+//       elementFilter: 'filter-btn',
+//       elementSort: 'sort-btn'
+//     },
+//     selectors: {
+//       target: '.mix-target'
+//     }
+//   });
+
+
+  var mixer = mixitup('.courses-mixed');

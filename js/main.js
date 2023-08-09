ecabe375c14cb7867023012b0d42b3a08f4ff95d
emonhossen07 <anashossainemon@gmail.com>
2023-08-09 22:00:04 +0600

    home page- notice board section, about section, our investors section, our partners section and also fix some img issues in course section also course page

--- js/main.js
+++ js/main.js
@@ -63,3 +63,37 @@ $('.stop').on('click', function () {
 // mixitup setup
 
 var mixer = mixitup('.mixitup-setup');
+
+
+
+
+/**
+ * Clients Slider
+ */
+new Swiper('.clients-slider', {
+    speed: 400,
+    loop: true,
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    breakpoints: {
+        320: {
+            slidesPerView: 2,
+            spaceBetween: 40
+        },
+        480: {
+            slidesPerView: 3,
+            spaceBetween: 60
+        },
+        640: {
+            slidesPerView: 4,
+            spaceBetween: 80
+        },
+        992: {
+            slidesPerView: 6,
+            spaceBetween: 120
+        }
+    }
+});
